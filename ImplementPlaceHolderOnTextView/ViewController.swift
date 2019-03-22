45cbb7572555d091578c2d7f3e5439ccf302e683
aki0207 <forever.look.aki@docomo.ne.jp>
2019-03-22 17:35:28 +0900

    @IBInspectable未使用

--- ImplementPlaceHolderOnTextView/ViewController.swift
+++ ImplementPlaceHolderOnTextView/ViewController.swift
@@ -1,20 +1,33 @@
-//
-//  ViewController.swift
-//  ImplementPlaceHolderOnTextView
-//
-//  Created by ききょ on 2019/03/22.
-//  Copyright © 2019 kikyo. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextViewDelegate {
 
+    
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        textView.delegate = self
+        
     }
-
-
+    
+    
+    //フォーカスが当たったときはプレースホルダー非表示
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textLabel.isHidden = true
+    }
+    
+    
+    //textviewからフォーカスが外れて、TextViewが空だったらLabelを再び表示
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if(textView.text.isEmpty){
+            textLabel.isHidden = false
+        }
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
 
